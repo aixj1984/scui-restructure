703a710b4b25ba003f960432d0ee0664b3814bd5
xiongjun ai <xiongjun.ai@guasemi.com>
2023-12-13 18:38:40 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/static/' : '',
+  base: process.env.VITE_NODE_ENV === 'production' ? '/static/' : '',
   plugins: [vue()],
   resolve: {
     alias: {
